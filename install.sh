e3db36d5bb3a670ffa8d75b54cee38fb2a58c11d
Brett Smith <brettcsmith@brettcsmith.org>
2013-12-29 10:06:32 -0500

    install.sh: Better handle customized files.
    For files that normally have settings added to them, instead of
    prompting every time, assume they're good as long as the installed
    version starts with the contents of the provided version.

--- install.sh
+++ install.sh
@@ -14,12 +14,17 @@ my_find() {
 
 ask_overwrite() {
     local fn=$1; shift
+    local default_answer=${1:-N}
+    case $default_answer in
+        Y) local answers="(Y/n)" ;;
+        *) local answers="(y/N)" ;;
+    esac
     while true; do
 	echo
 	grep -viE '\bpass(word|wd|)\b' "$fn" | head -n 10
-	printf "OK to overwrite this $fn? (y/N) "
+	printf "OK to overwrite this $fn? $answers "
 	read ans
-	ans=$(echo "${ans:-n}" | tr '[:upper:]' '[:lower:]')
+	ans=$(echo "${ans:-$default_answer}" | tr '[:upper:]' '[:lower:]')
 	if [ "$ans" = n ] || [ "$ans" = no ]; then
 	    return 1
 	elif [ "$ans" = y ] || [ "$ans" = yes ]; then
@@ -31,9 +36,20 @@ ask_overwrite() {
 can_overwrite() {
     local fn=$1; shift
     local target=$1; shift
+    local overwrite_default=N
     test -e "$target" || return 0
-    cmp -s "$fn" "$target" && return 1
-    ask_overwrite "$target"
+    case "$fn" in
+        # Files that start with standard settings
+        .gitconfig|.hgrc|.mutt/muttrc)
+            local linecount=$(wc -l <"$fn")
+            head -n "$linecount" "$target" | cmp -s "$fn" - && return 1
+            ;;
+        *)
+            cmp -s "$fn" "$target" && return 1
+            overwrite_default=Y
+            ;;
+    esac
+    ask_overwrite "$target" $overwrite_default
 }
 
 destdir=${1:-$HOME}

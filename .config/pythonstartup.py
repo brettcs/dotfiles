bf8eae92da9ce9cfce86258fcd86b66cdd21ba39
Brett Smith <brettcsmith@brettcsmith.org>
2018-01-08 11:14:54 -0500

    pythonstartup: New file for convenient Python shells.

---
+++ .config/pythonstartup.py
@@ -0,0 +1,34 @@
+import datetime
+import decimal
+import functools
+import io
+import itertools
+import operator
+import os
+import random
+import re
+import subprocess
+import sys
+import tempfile
+import traceback
+import unicodedata
+
+try:
+    import pathlib
+except ImportError:
+    pass
+else:
+    Path = pathlib.Path
+
+Date = datetime.date
+DateTime = datetime.datetime
+TimeDelta = datetime.timedelta
+TZInfo = datetime.tzinfo
+Decimal = decimal.Decimal
+StringIO = io.StringIO
+
+try:
+    TimeZone = datetime.timezone
+    UTC = datetime.timezone.utc
+except AttributeError:
+    pass
